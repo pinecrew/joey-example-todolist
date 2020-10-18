a8d0cd2005f16177192949b730b7c60a05b4ad3d
Vova Abdrakhmanov <369565@gmail.com>
2020-10-18 19:01:34 +0400

    Add CORS middleware

--- asgi.py
+++ asgi.py
@@ -1,4 +1,7 @@
 from joey import Application
 from settings import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = Application(settings)
+
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
