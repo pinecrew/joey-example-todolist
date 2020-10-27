cbac20a297de2f0712f62c61ec49d2ab0abaae0c
Alexey Golubev <dr.freecx@gmail.com>
2020-10-28 01:19:18 +0400

    add simple login/logout form

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -2,13 +2,33 @@ const app = Vue.createApp({
   data() {
     return {
       todo_lists: [],
-      todo_items: []
+      todo_items: [],
+      login: '',
+      password: '',
+      username: ''
     }
   },
   async created() {
-    this.todo_lists = await axios.get('/lists')
-                                 .then((r) => { return r.data; });
+    this.todo_lists = await axios.get('/lists').then((r) => { return r.data; });
+    let username = window.localStorage.getItem('username');
+    this.username = username ? username : '';
   },
+  methods: {
+    async process_login() {
+      await axios.post('/login', { login: this.login, password: this.password }).then((r) => {
+        this.username = r.data.name;
+        window.localStorage.setItem('username', r.data.name);
+      })
+      this.todo_lists = await axios.get('/lists').then((r) => { return r.data; });
+    },
+    async process_logout() {
+      await axios.get('/logout');
+      window.localStorage.removeItem('username');
+      this.username = '';
+      this.todo_lists = [];
+      this.todo_items = [];
+    }
+  }
 })
 
 app.component('todo-lists', {
