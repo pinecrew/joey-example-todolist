ed4e638ccf2bf57f39ea92d67fe280f166ecee01
Alexey Golubev <dr.freecx@gmail.com>
2020-10-28 11:20:14 +0400

    update client code & style

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -9,24 +9,49 @@ const app = Vue.createApp({
     }
   },
   async created() {
-    this.todo_lists = await axios.get('/lists').then((r) => { return r.data; });
-    let username = window.localStorage.getItem('username');
-    this.username = username ? username : '';
+    this.load_todo_list();
+    this.username = this.username_get();
   },
   methods: {
+    username_set(username) {
+      this.username = username;
+      window.localStorage.setItem('username', username);
+    },
+    username_get() {
+      let username = window.localStorage.getItem('username');
+      return username ? username : '';
+    },
+    username_reset() {
+      this.username = '';
+      window.localStorage.removeItem('username');
+    },
+    async load_todo_list() {
+      this.todo_lists = await axios.get('/lists')
+        .then((r) => { return r.data; })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     async process_login() {
-      await axios.post('/login', { login: this.login, password: this.password }).then((r) => {
-        this.username = r.data.name;
-        window.localStorage.setItem('username', r.data.name);
-      })
-      this.todo_lists = await axios.get('/lists').then((r) => { return r.data; });
+      await axios.post('/login', { login: this.login, password: this.password })
+        .then((r) => {
+          this.username_set(r.data.name);
+          this.login = '';
+          this.password = '';
+          this.load_todo_list();
+        }).catch((error) => {
+          console.error(error);
+        })
     },
     async process_logout() {
-      await axios.get('/logout');
-      window.localStorage.removeItem('username');
-      this.username = '';
-      this.todo_lists = [];
-      this.todo_items = [];
+      await axios.get('/logout')
+        .then(() => {
+          this.username_reset();
+          this.todo_lists = [];
+          this.todo_items = [];
+        }).catch((error) => {
+          console.log(error);
+        })
     }
   }
 })
@@ -35,17 +60,20 @@ app.component('todo-lists', {
   methods: {
     async update(id) {
       this.$parent.todo_items = await axios.get(`/lists/${id}/items`)
-                                           .then((r) => { return r.data });
+        .then((r) => { return r.data });
     }
   },
   template: `
-    <p @click="update(list.id)">#{{ list.id }} {{ list.title }} by {{ list.owner ? list.owner : "unknown" }}</p>
+    <input id="button_{{ list.id }}" name="lists" type="radio" @click="update(list.id)">
+    <label for="button_{{ list.id }}">
+      #{{ list.id }} {{ list.title }} by {{ list.owner ? list.owner : "unknown" }}
+    </label><br>
   `,
   props: ['list'],
 })
 app.component('todo-items', {
   template: `
-    <p>#[{{ item.id }}, {{ item.todolist }}] {{ item.value }} [{{ item.status }}]</p>
+    <p class="items">#[{{ item.id }}, {{ item.todolist }}] {{ item.value }} [{{ item.status }}]</p>
   `,
   props: ['item']
 })
