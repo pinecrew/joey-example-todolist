45cda3e7030b75b3f66586811be31f084451a5c8
Vova Abdrakhmanov <369565@gmail.com>
2020-11-08 18:14:01 +0400

    Add some checks

--- todolist/auth.py
+++ todolist/auth.py
@@ -4,11 +4,13 @@ def login(scope, user):
     have to reauthenticate on every request. Note that data set during
     the anonymous session is retained when the user logs in.
     """
-    scope['session']['user_id'] = user.id
+    if 'session' in scope and isinstance(scope['session'], dict):
+        scope['session']['user_id'] = user.id
 
 def logout(scope):
     """
     Remove the authenticated user's ID from the request and flush their session
     data.
     """
-    del scope['session']['user_id']
+    if 'session' in scope and 'user_id' in scope['session']:
+        del scope['session']['user_id']
